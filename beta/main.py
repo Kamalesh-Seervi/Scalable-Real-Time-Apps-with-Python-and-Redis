29a8387b0f536f0ba7d871b57369c0136672dba1
kd14 <kd14@ford.com>
2024-04-27 21:11:31 +0530

    uvicorn_setup

--- beta/main.py
+++ beta/main.py
@@ -2,7 +2,8 @@ import asyncio
 from fastapi import FastAPI
 from dataclasses import dataclass
 from typing import Dict
-
+import requests
+import uvicorn
 
 
 app = FastAPI()
@@ -22,9 +23,25 @@ users:Dict[int,data]={
 # print(users[1])
 
 @app.get("userdata/{user_id}")
-async def read_item(user_id: int):
+async def get_user(user_id: int):
     # for testing purpose i am doing 0.2
     await asyncio.sleep(0.2)
     if user_id in users:
         return {"ok": True, "user": users[user_id]}
     return {"ok": False, "error": "user not there"}
+
+# def get_user_info(user_id: int) -> dict | None:
+#     response=requests.get(f"http://127.0.0.1:8000/user/{user_id}").json()
+#     return response
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
+    
+    
+    
